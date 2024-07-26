e73361c180c9449545f5cf591f4ea0dc30074255
Ritvik <ritvik.gupta2205@gmail.com>
2024-07-27 01:10:29 +0530

    changing size of logo

--- website/static/main.js
+++ website/static/main.js
@@ -1,19 +1,22 @@
 $(document).ready(function() {
-    $(window).on('scroll', function() {
-        const $image = $('img');
-        const scrollPosition = $(window).scrollTop();
-        const scale = Math.max(1 - scrollPosition / 1000, 0.5);
+
     
-        $image.css('transform', `scale(${scale})`);
-        
-        const initialTop = ($(window).height() - $image.height()) / 2;
-        const finalTop = -70;
-        const top = Math.max(finalTop, initialTop - scrollPosition);
+});
+
+$(window).on('scroll', function() {
+    const $image = $('img');
+    const scrollPosition = $(window).scrollTop();
+    const scale = Math.max(1 - scrollPosition / 1000, 0.5);
+
+    $image.css('transform', `scale(${scale})`);
     
-    
-        $image.css({
-            'position': 'fixed',
-            'top': `${Math.max(top, -70)}px`
-        }); 
-    });
+    const initialTop = ($(window).height() - $image.height()) / 2;
+    const finalTop = -70;
+    const top = Math.max(finalTop, initialTop - scrollPosition);
+
+
+    $image.css({
+        'position': 'fixed',
+        'top': `${Math.max(top, -70)}px`
+    }); 
 });
