e0e7374f568f0033cafccb72b3d6f09abbfa767c
Ritvik <ritvik.gupta2205@gmail.com>
2024-07-27 22:18:32 +0530

    adding offset margin for logo

--- website/static/main.js
+++ website/static/main.js
@@ -4,14 +4,15 @@ $(document).ready(function() {
 });
 
 $(window).on('scroll', function() {
-    const $image = $('img');
+    const $image = $('#logo');
     const scrollPosition = $(window).scrollTop();
     const scale = Math.max(1 - scrollPosition / 1000, 0.5);
+    const offset = 100;
 
     $image.css('transform', `scale(${scale})`);
     
-    const initialTop = ($(window).height() - $image.height()) / 2;
-    const finalTop = -70;
+    const initialTop = (($(window).height() - $image.height()) / 2) - offset;
+    const finalTop = -50;
     const top = Math.max(finalTop, initialTop - scrollPosition);
 
 
