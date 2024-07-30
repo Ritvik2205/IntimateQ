abcbeb3addcc6d17769813a3a20f2141ea34c549
Ritvik <ritvik.gupta2205@gmail.com>
2024-07-30 12:08:56 +0530

    ading smooth scrolling

--- website/static/main.js
+++ website/static/main.js
@@ -1,5 +1,18 @@
 $(document).ready(function() {
 
+    // Add smooth scrolling to all links
+    $(".tab a").on('click', function(event) {
+        if (this.hash !== "") {
+            // Prevent default anchor click behavior
+            event.preventDefault();
+            const hash = this.hash;
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800, function() {
+                window.location.hash = hash;
+            });
+        } 
+    });
     
 });
 
