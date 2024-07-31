89731ae3b81e4b13f77bc244c06cd0583f1b3561
Ritvik <ritvik.gupta2205@gmail.com>
2024-07-31 16:54:25 +0530

    fixing heights for services section

--- website/static/main.js
+++ website/static/main.js
@@ -21,13 +21,13 @@ $(document).ready(function() {
 $(window).on('scroll', function() {
     const $image = $('#logo');
     const scrollPosition = $(window).scrollTop();
-    const scale = Math.max(1 - scrollPosition / 1000, 0.5);
+    const scale = Math.max(1 - scrollPosition / 1000, 0.4);
     const offset = 90;
 
     $image.css('transform', `scale(${scale})`);
     
     const initialTop = (($(window).height() - $image.height()) / 2) - offset;
-    const finalTop = -50;
+    const finalTop = -70;
     const top = Math.max(finalTop, initialTop - scrollPosition);
 
 
