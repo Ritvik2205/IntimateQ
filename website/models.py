d8a6d81fc365948c72f9a1ab37f1828ad70e45fc
Ritvik <ritvik.gupta2205@gmail.com>
2024-08-20 01:15:18 +0530

    adding MySQL model for user

---
+++ website/models.py
@@ -0,0 +1,13 @@
+from . import db
+from flask_login import UserMixin
+from sqlalchemy.sql import func
+
+
+class User(db.Model, UserMixin):
+    __tablename__ = 'user'
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(150), unique=True)
+    username = db.Column(db.String(150), unique=True)
+    password = db.Column(db.String(150))
+    dateOfBirth = db.Column(db.DateTime(timezone=True), default=func.now())
+    gender = db.Column(db.String(150))
