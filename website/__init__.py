a318162c526b881563b9664edd964d37f980b352
Ritvik <ritvik.gupta2205@gmail.com>
2024-08-23 00:07:07 +0530

    minor env fixes

--- website/__init__.py
+++ website/__init__.py
@@ -10,17 +10,17 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-MYSQL_DB_NAME = getenv("MYSQL_DB_NAME")
-MYSQL_USERNAME = getenv("MYSQL_USERNAME")
-MYSQL_PASSWORD = getenv("MYSQL_PASSWORD")
-MYSQL_PORT = getenv("MYSQL_PORT")
-MYSQL_HOST = getenv("MYSQL_HOST")
+MYSQL_DB_NAME = getenv("MYSQL_DB_NAME").strip()
+MYSQL_USERNAME = getenv("MYSQL_USERNAME").strip()
+MYSQL_PASSWORD = getenv("MYSQL_PASSWORD").strip()
+MYSQL_PORT = int(getenv("MYSQL_PORT").strip())
+MYSQL_HOST = getenv("MYSQL_HOST").strip()
 
-SESSION_TYPE = getenv("SESSION_TYPE")
-SESSION_REDIS_HOST = getenv("SESSION_REDIS_HOST")
-SESSION_REDIS_PORT = getenv("SESSION_REDIS_PORT")
-REDIS_USERNAME = getenv("REDIS_USERNAME")
-REDIS_PASSWORD = getenv("REDIS_PASSWORD")
+SESSION_TYPE = getenv("SESSION_TYPE").strip()
+SESSION_REDIS_HOST = getenv("SESSION_REDIS_HOST").strip()
+SESSION_REDIS_PORT = int(getenv("SESSION_REDIS_PORT").strip())
+REDIS_USERNAME = getenv("REDIS_USERNAME").strip()
+REDIS_PASSWORD = getenv("REDIS_PASSWORD").strip()
 
 
 db = SQLAlchemy()
@@ -36,13 +36,16 @@ def create_app():
 
     app.config['SQLALCHEMY_DATABASE_URI'] = \
             f'mysql+pymysql://{MYSQL_USERNAME}:{MYSQL_PASSWORD}@{MYSQL_HOST}:{MYSQL_PORT}/{MYSQL_DB_NAME}'
-    
+    app.config['SQLALCHEMY_ENGINE_OPTIONS'] = {
+        'pool_pre_ping': True,
+        'pool_recycle': 280
+    }
 #     app.config['MONGO_URI'] = f'mongodb://localhost:27017/intimateQ'
 
     #configuring redis
     app.config['SESSION_TYPE'] = SESSION_TYPE
-    app.config['SESSION_PERMANENT'] = False
-    app.config['SESSION_USE_SIGNER'] = True
+#     app.config['SESSION_PERMANENT'] = False
+#     app.config['SESSION_USE_SIGNER'] = True
     app.config['SESSION_REDIS'] = redis.StrictRedis(
                         host=SESSION_REDIS_HOST,
                         port=SESSION_REDIS_PORT,
